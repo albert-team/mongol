56d0ca0473dba0819bcfb4c8419c03d329888f25
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-02-14 16:23:55 +0700

    autoTimestamp hook supports bulkwrite now

--- src/builtins/hooks.ts
+++ src/builtins/hooks.ts
@@ -1,3 +1,4 @@
+import { REPLACE_OPERATIONS, UPDATE_OPERATIONS } from '../constants'
 import {
   AutoTimestampOptions,
   CrudOperation,
@@ -10,36 +11,48 @@ export const autoTimestamp = (options: AutoTimestampOptions): DatabaseHook => {
   const {
     namingConventions = [NamingConvention.Unchanged, NamingConvention.CamelCase]
   } = options
-  const dbNC = namingConventions[1]
+  const [, dbNC] = namingConventions
+  const caPropName = dbNC === NamingConvention.SnakeCase ? 'created_at' : 'createdAt'
+  const uaPropName = dbNC === NamingConvention.SnakeCase ? 'updated_at' : 'updatedAt'
 
   return {
     before: (context, ...args) => {
       const { operation: op } = context
 
-      if ([CrudOperation.FindOneAndReplace, CrudOperation.ReplaceOne].includes(op)) {
-        // args = filter, replacement, options
-        const propName = dbNC === NamingConvention.SnakeCase ? 'updated_at' : 'updatedAt'
-        args[1] = withTimestamp(args[1], propName)
-      } else if (
-        [
-          CrudOperation.FindOneAndUpdate,
-          CrudOperation.UpdateMany,
-          CrudOperation.UpdateOne
-        ].includes(op)
-      ) {
-        // args = filter, update, options
-        const propName = dbNC === NamingConvention.SnakeCase ? 'updated_at' : 'updatedAt'
-        args[1] = { ...args[1], $currentDate: { [propName]: true } }
-      } else if (op === CrudOperation.InsertMany) {
-        // args = docs, options
-        const propName = dbNC === NamingConvention.SnakeCase ? 'created_at' : 'createdAt'
-        args[0] = args[0].map((doc) => withTimestamp(doc, propName))
-      } else if (op === CrudOperation.InsertOne) {
+      if (op === CrudOperation.InsertOne)
         // args = doc, options
-        const propName = dbNC === NamingConvention.SnakeCase ? 'created_at' : 'createdAt'
-        args[0] = withTimestamp(args[0], propName)
-      }
-
+        args[0] = withTimestamp(args[0], caPropName)
+      else if (op === CrudOperation.InsertMany)
+        // args = docs, options
+        args[0] = args[0].map((doc) => withTimestamp(doc, caPropName))
+      else if (UPDATE_OPERATIONS.includes(op))
+        // args = filter, update, options
+        args[1] = { ...args[1], $currentDate: { [uaPropName]: true } }
+      else if (REPLACE_OPERATIONS.includes(op))
+        // args = filter, replacement, options
+        args[1] = withTimestamp(args[1], uaPropName)
+      else if (op === CrudOperation.BulkWrite)
+        // args = operations, options
+        args[0] = args[0].map((subOp) => {
+          if (subOp.insertOne)
+            subOp.insertOne.document = withTimestamp(subOp.insertOne.document, caPropName)
+          else if (subOp.updateOne)
+            subOp.updateOne.update = {
+              ...subOp.updateOne.update,
+              $currentDate: { [uaPropName]: true }
+            }
+          else if (subOp.updateMany)
+            subOp.updateMany.update = {
+              ...subOp.updateMany.update,
+              $currentDate: { [uaPropName]: true }
+            }
+          else if (subOp.replaceOne)
+            subOp.replaceOne.replacement = withTimestamp(
+              subOp.replaceOne.replacement,
+              uaPropName
+            )
+          return subOp
+        })
       return args
     }
   } as DatabaseHook
